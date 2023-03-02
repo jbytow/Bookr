9c80adfdaa16193cd7880881d10923d2f45f3706
Jakub <jakub.bytow@gmail.com>
2023-03-02 10:36:22 +0100

    0.01

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    name = "world"
+    return render(request, "base.html", {"name": name})
+
+
+def book_search(request):
+    search_text = request.GET.get("search", "")
+    return render(request, "search-results.html", {"search_text": search_text})
