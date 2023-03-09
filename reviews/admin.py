6cd5862d2747670613fa1aa8d2748c9ef27d714b
Jakub <jakub.bytow@gmail.com>
2023-03-09 10:16:22 +0100

    0.04 admin panel WIP

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,17 +1,34 @@
-from django.contrib.admin import AdminSite
+from django.contrib import admin
 from reviews.models import Publisher, Contributor, Book, BookContributor, Review
 
 
-class BookrAdminSite(AdminSite):
-    title_header = 'Bookr Admin App'
-    site_header = 'Bookr Admin App'
-    index_title = 'Bookr Admin App'
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'isbn13')
+    list_filter = ('publisher',)
 
-admin_site=BookrAdminSite(name='bookr')
+    def isbn13(self, obj):
+        """ '9780316769174' => '978-0-31-676917-4' """
+        return "{}-{}-{}-{}-{}".format(obj.isbn[0:3], obj.isbn[3:4],
+                                       obj.isbn[4:6], obj.isbn[6:12],
+                                       obj.isbn[12:13])
 
-# Register your models here
+
+def initialled_name(obj):
+    """ obj.first_names='Jerome David', obj.last_names='Salinger'
+        => 'Salinger, JD' """
+    initials = ''.join([name[0] for name in obj.first_names.split(' ')])
+    return "{}, {}".format(obj.last_names, initials)
+
+
+class ContributorAdmin(admin.ModelAdmin):
+    list_display = (initialled_name,)
+
+
+
+
+# Register your models here.
 admin.site.register(Publisher)
 admin.site.register(Contributor)
-admin.site.register(Book)
+admin.site.register(Book, BookAdmin)
 admin.site.register(BookContributor)
 admin.site.register(Review)
