3cf3b917e7257118aabbb83432c5955325a7aea6
Jakub <jakub.bytow@gmail.com>
2023-03-28 12:45:33 +0200

    0.25 Admin panel update

--- reviews/admin.py
+++ reviews/admin.py
@@ -3,16 +3,12 @@ from reviews.models import Publisher, Contributor, Book, BookContributor, Review
 
 
 class BookAdmin(admin.ModelAdmin):
-    date_hierarchy = 'publication_date'
-    list_display = ('title', 'isbn13')
-    list_filter = ('publisher', 'publication_date')
-    search_fields = ('title', 'isbn', 'publisher_name')
+    model = Book
+    list_display = ('title', 'isbn', 'get_publisher', 'publication_date')
+    search_fields = ['title', 'publisher__name']
 
-    def isbn13(self, obj):
-        """ '9780316769174' => '978-0-31-676917-4' """
-        return "{}-{}-{}-{}-{}".format(obj.isbn[0:3], obj.isbn[3:4],
-                                       obj.isbn[4:6], obj.isbn[6:12],
-                                       obj.isbn[12:13])
+    def get_publisher(self, obj):
+        return obj.publisher.name
 
 
 def initialled_name(obj):
